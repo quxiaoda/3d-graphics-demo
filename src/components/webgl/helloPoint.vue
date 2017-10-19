11c4667827bc451cca92f1bf10eada9562b57319
曲小达 <quda01@sina.com>
2017-10-19 22:19:42 +0800

    add iview && helloPoint updated

--- src/components/webgl/helloPoint.vue
+++ src/components/webgl/helloPoint.vue
@@ -4,11 +4,40 @@
       您的浏览器不支持canvas！
     </canvas>
 
-    <div class="option">
-      
-    </div>  
+    <ul class="option">
+      <li>
+        <span class="text">X: </span>
+        <slider class="slider" v-model="x" :min="0" :max="10" :tip-format="hideFormat" @on-input="setX"></slider>
+      </li>
+      <li>
+        <span class="text">Y: </span>
+        <slider class="slider" v-model="y" :min="0" :max="10" :tip-format="hideFormat" @on-input="setY"></slider>
+      </li>
+      <li>
+        <span class="text">Z: </span>
+        <slider class="slider" v-model="z" :min="0" :max="10" :tip-format="hideFormat" @on-input="setZ"></slider>
+      </li>
+    </ul>  
   </div>
 </template>
+
+<style type="scss">
+  .option{
+    width: 400px;
+    margin: 0 auto;
+  }
+  .text{
+    display: inline-block;
+    font-size: 16px;
+    height: 34px;
+    vertical-align: text-bottom;
+  }
+  .slider{
+    display: inline-block;
+    width: 300px;
+    margin-left: 10px;
+  }
+</style>
 
 <script>
   import '../base/reset.css'
@@ -21,8 +50,43 @@
         x: 0.0,
         y: 0.0,
         z: 0.0,
-        size: 10.0
+        size: 10.0,
+        gl: null,
+        X: 0,
+        Y: 0,
+        Z: 0,
       }
+    },
+    methods: {
+        hideFormat: function(val) {
+            return val/10;
+        },
+        setX: function(value) {
+          this.renderPoint({x: value/10})
+          console.log(value)
+        },
+        setY: function(value) {
+          this.renderPoint({y: value/10})
+          console.log(value)
+        },
+        setZ: function(value) {
+          this.renderPoint({z: value/10})
+          console.log(value)
+        },
+        renderPoint: function({x, y, z}) {
+          let gl = this.gl
+
+          x && (this.X = x)
+          y && (this.Y = y)
+          z && (this.Z = z)
+
+          gl.clearColor(0, 0, 0, 1)
+          gl.clear(gl.COLOR_BUFFER_BIT)
+          gl.vertexAttrib3f(this.a_Position, this.X, this.Y, this.Z)
+          // gl.vertexAttrib1f(this.a_PointSize, 40.0)
+          // 绘制一个点
+          gl.drawArrays(gl.POINTS, 0, 1)
+        }
     },
   	mounted() {
       // 顶点着色器
@@ -42,6 +106,7 @@
         '}\n';
 
       let gl = getWebGLContext('webgl')
+      this.gl = gl //全局变量
 
       // 初始化着色器
       if(!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)){
@@ -52,11 +117,13 @@
       let a_Position = gl.getAttribLocation(gl.program, 'a_Position')
       let a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize')
 
+      this.a_Position = a_Position
+      this.a_PointSize = a_PointSize
+
       if (a_Position < 0) {
         console.log('Failed to get the storage location of a_Position')
         return
       }
-
       gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.9)
       gl.vertexAttrib1f(a_PointSize, 40.0)
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';    // 使用 CSS
 
 Vue.config.productionTip = false
+
+Vue.use(iView);
 
 /* eslint-disable no-new */
 new Vue({
